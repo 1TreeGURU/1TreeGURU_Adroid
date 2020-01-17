8061b4b1106fc45e897c3302df7d266ef1ff7f4a
AHN-GAHEE <liss4936@gmail.com>
2020-01-17 15:02:16 +0900

    initial commit

--- app/src/main/java/com/example/mylib/LibraryMain.java
+++ app/src/main/java/com/example/mylib/LibraryMain.java
@@ -1,7 +1,9 @@
 package com.example.mylib;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +14,7 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import java.util.Calendar;
 import java.util.Collection;
 
-public class MainActivity extends AppCompatActivity {
+public class LibraryMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         MaterialCalendarView materialCalendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
 
+
+//       캘린더 첫날과 마지막 날 지정
         materialCalendarView.state().edit()
                 .setFirstDayOfWeek(Calendar.SUNDAY)
                 .setMinimumDate(CalendarDay.from(2017, 0, 1))
@@ -28,13 +32,31 @@ public class MainActivity extends AppCompatActivity {
                 .setCalendarDisplayMode(CalendarMode.MONTHS)
                 .commit();
 
+
+//      주말과 오늘 날짜 색깔 지정
         final OneDayDecorator oneDayDecorator = new OneDayDecorator();
         materialCalendarView.addDecorators(
                 new SundayDecorator(),
                 new SaturdayDecorator(),
                 oneDayDecorator);
 
-        Collection<CalendarDay> calendarDays;
-        materialCalendarView.addDecorator(new EventDecorator(Color.RED, calendarDays);
+
+//       글 작성한 날에 점 표시 (미구현)
+        Collection<CalendarDay> calendarDays = null;
+        CalendarDay calendarDay = new CalendarDay();
+        calendarDays.add(calendarDay);
+//        materialCalendarView.addDecorator(new EventDecorator(Color.RED, calendarDays);
+
+
+//        내 서재 > 설정
+        Button btn_lib_setting = (Button)findViewById(R.id.btn_lib_setting);
+        btn_lib_setting.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), LibrarySetting.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
---
+++ app/src/main/java/com/example/mylib/LibrarySetting.java
@@ -0,0 +1,29 @@
+package com.example.mylib;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LibrarySetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_setting);
+
+
+//       내 서재 설정 저장 버튼 리스너
+        Button btn_lib_save = (Button)findViewById(R.id.btn_lib_save);
+        btn_lib_save.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), LibraryMain.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
