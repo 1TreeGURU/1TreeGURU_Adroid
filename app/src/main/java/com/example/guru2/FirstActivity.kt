897cfea762f470b71a92b9abd3062f5da5a85db8
JIWONKIM <qhsh866@gmail.com>
2020-01-19 17:07:39 +0900

    merge login, first activity

--- app/src/main/java/com/example/guru2/FirstActivity.kt
+++ app/src/main/java/com/example/guru2/FirstActivity.kt
@@ -3,7 +3,6 @@ package com.example.guru2
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_first.*
 
